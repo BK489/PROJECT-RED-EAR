5459d008d15a0bef960ab9ad49ed3da0010fd578
BK489 <beck.lambert@outlook.com>
2025-01-12 18:20:22 +0000

    dwnload dir gui added + styleloading fixed

--- app/tkinter_app.py
+++ app/tkinter_app.py
@@ -10,20 +10,22 @@ class App:
     def __init__(self):
         #main window
         self.root = Tk()
-        self.root.title("RED-EAR v1.0-ALPHA")
-        self.root.geometry("500x300")
+        self.root.title("RED-EAR v1.0.0-ALPHA")
+        self.root.geometry("600x400")
         self.root.resizable(False, False)
         self.root.iconbitmap(False, "assets/red-ear-ico.ico")
         
         #background image
         img = Image.open("assets/red-ear-background.png")
-        background_image = ImageTk.PhotoImage(img.resize((500, 300)))
+        background_image = ImageTk.PhotoImage(img.resize((600, 400)))
         background_label = Label(self.root, image=background_image)
         background_label.image = background_image  # Keep a reference to avoid garbage collection
-        background_label.place(x=-2, y=0)
+        background_label.place(x=-1, y=0)
 
         #menu bar
         self.setup_menu()
+        #topframe
+        self.setup_widgets()
 
         #load ttk style
         load_style()
@@ -53,8 +55,21 @@ class App:
         self.root.config(menu=menu_bar)
 
     def setup_widgets(self):
-        #sets up all widgets on app
-        print("test")
+        #sets up widgets on application page
+        #title
+        title_label = Label(self.root, text="RED-EAR V1.0.0-ALPHA", font="terminal")
+        title_label.place(x=175, y=15)
+
+        #directory n config file
+        directory_text = Text(self.root, font="terminal 11", height=1, width=60)    
+        directory_text.place(x=20, y=53)
+        directory_text.insert("1.0", "C:/")
+        directory_label = Label(self.root, font="terminal 11", text="no config found")
+        directory_label.place(x=20,y=71)
+        choose_dir_button = Button(self.root, text="Choose Download Folder", style="Custom.TButton", command=lambda: print("choose dir pressed"))
+        choose_dir_button.place(x=350, y=50)
+        
+
 
     def run(self):
         #Runs the Tkinter main loop.
--- styles/gui_style.py
+++ styles/gui_style.py
@@ -3,20 +3,26 @@ from tkinter.ttk import Style
 def load_style():
 
     s = Style()
-    s.theme_use('classic') #Available themes: ('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
+    s.theme_use('default') #Available themes: ('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
     #configure for general, map for dynamic
 
-    #menubar
-    s.configure( 
-        "custom.TMenu",
-        background="#ff6666",
-        foreground="black",
-        padding=5
-    )
+    #buttons
+    s.configure(
+    "Custom.TButton",
+    font=("Terminal", 11),
+    background="firebrick1",
+    foreground="white",
+)
     s.map(
-        "custom.menubar",
-        background=[("active", ""), ("disabled", "lightgrey")],
-        foreground=[("disabled", "grey")]
-    )
+    "Custom.TButton",
+    background=[
+        ("active", "firebrick2"),
+        ("pressed", "firebrick4"),
+    ],
+    foreground=[
+        ("disabled", "gray"),
+    ],
+)
+
 
     return s
